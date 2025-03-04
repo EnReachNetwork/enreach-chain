0faae0efb0c17f056b3b286900a589b4961d90b0
BrianWu <wuhaixian1984@163.com>
2025-03-04 16:17:05 +0800

    Update the test script to add --chain-id param to the commands

--- testscripts/createregion.sh
+++ testscripts/createregion.sh
@@ -1,3 +1,3 @@
-/Users/brianwu/go/bin/enreachd tx registry create-region asia Asia AsiaRegion --from alice
+/Users/brianwu/.gvm/pkgsets/go1.23.3/global/bin/enreachd tx registry create-region asia Asia AsiaRegion --from alice --chain-id enreach
 sleep 1
-/Users/brianwu/go/bin/enreachd tx registry create-region eu europe EuropeRegion --from bob
+/Users/brianwu/.gvm/pkgsets/go1.23.3/global/bin/enreachd tx registry create-region eu europe EuropeRegion --from bob --chain-id enreach
